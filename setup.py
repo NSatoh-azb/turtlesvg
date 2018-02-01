1d866a298df2b45ca413eff08a8fb2d55396531e
N.Satoh <nsatoh_3_1415@hotmail.co.jp>
2018-02-01 10:23:23 +0900

    バージョンを更新．

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='turtlesvg',
-    version='0.1',
+    version='1.0',
     author='SATOH Naotaka',
     py_modules=['turtlesvg'],
     packages=['svgutl'],
