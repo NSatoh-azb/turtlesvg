8e7b09fdfe610566df7c863d90947bbfa62b7f7a
N.Satoh <nsatoh@azabu-jh.ed.jp>
2020-02-15 15:21:46 +0900

    turtlesvgを独立パッケージとして切り分け．ディレクトリ構成整理．

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='turtlesvg',
+    version='1.4',
+    author='SATOH Naotaka',
+    py_modules=['turtlesvg'],
+    packages=['svgutl'],
+    package_dir={'':'turtlesvg'}
+    )
