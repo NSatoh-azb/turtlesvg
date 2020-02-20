96accfc29c03c405cfbb2277ce8fd64510f65ee8
N.Satoh <nsatoh@azabu-jh.ed.jp>
2020-02-20 12:52:51 +0900

    設定ファイルの編集．

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='turtlesvg',
-    version='1.4',
+    version='1.5',
     author='SATOH Naotaka',
     py_modules=['turtlesvg'],
     packages=['svgutl'],
